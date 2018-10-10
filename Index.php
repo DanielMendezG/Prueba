8a3cedb7fad482e86f72a755cb09f666aaa0af87
DanielMendezG <43685576+DanielMendezG@users.noreply.github.com>
2018-10-10 10:49:08 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIDES</title>
+	<link rel="icon" type="img/png" href="Vista/img/logo.png" />
+	<link rel="stylesheet" type="text/css" href="Vista/css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="Vista/css/style.css">
+	<script src="Vista/js/jQuery/jquery-3.3.1.js"></script>
+	<script src="Vista/js/FuncionToggle.js"></script>
+	<script src="Vista/js/FuncionNav.js"></script>
+</head>
+<body>
+	<span class="ir-arriba icon-chevron-up"></span>
+	<div class="responsive-bar">
+		<div class="logo">
+			<img src="Vista/img/logo.png">
+		</div>
+		<div class="menu">
+			<div class="toggle">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>
+
+	<nav>
+		<div class="logo">
+			<img src="Vista/img/logo.png">
+			<!--<h3>SIDES</h3>-->
+		</div>
+			<ul>
+				<li><a href="#" class="active">Inicio</a></li>
+				<li><a href="#">Proyecto SIDES</a></li>
+				<li><a href="#">Empresa</a></li>
+
+			</ul>
+	</nav>
+	<section class="b1">
+		<div class="in1">
+			<div class="content">
+				<h1 class="h1">BIENVENIDO</h1>
+				<a class="boton" href="#insert">Proyecto SIDES</a>
+			</div>
+		</div>
+	</section>
+	
+	<div class="Somos">
+		
+		<h2>Quienes somos</h2>
+		<div class="izquierda">
+			<p class="p"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+			<a href="#" class="mas">leer mas...</a></p>
+		</div>
+		<div class="derecha">
+			<img class="sides" src="Vista/img/logo.png" >
+		</div>
+		<h2 class="realizado">PROYECTO REALIZADO POR:</h2>
+			<div class="grupo">
+				<div class="container">
+					<img src="Vista/img/mujer.jpg" alt="" class="imagen">
+						<div class="nombre">
+							LISETH MONROY
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">LISETH MONROY
+								<p class="detalles">Semillero Developer & geeks</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/hombre.jpg" alt="" class="imagen">
+						<div class="nombre">
+							HAROL SANCHEZ
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">HAROL SANCHEZ
+								<p class="detalles">Semillero Developer & geeks</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/hombre.jpg" alt="" class="imagen">
+						<div class="nombre">
+							DANIEL MENDEZ
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">DANIEL MENDEZ
+								<p class="detalles">Semillero Developer & geeks</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/hombre.jpg" alt="" class="imagen">
+						<div class="nombre">
+							JULIAN MEDINA
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">JULIAN MEDINA
+								<p class="detalles">Semillero Developer & geeks</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/mujer.jpg" alt="" class="imagen">
+						<div class="nombre">
+							LILIBETH
+							<p class="detalles">Semillero GIAT</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">LILIBETH
+								<p class="detalles">Semillero GIAT</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/mujer.jpg" alt="" class="imagen">
+						<div class="nombre">
+							XIOMARA
+							<p class="detalles">Semillero GIAT</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">XIOMARA
+								<p class="detalles">Semillero GIAT</p>
+							</div>
+
+					</div>
+				</div>
+
+			</div>
+		
+			<h2 class="realizado">INSTRUCTORES DE APOYO:</h2>
+			<div class="grupo">
+				<div class="container">
+					<img src="Vista/img/hombre.jpg" alt="" class="imagen">
+						<div class="nombre">
+							JESUS ARIEL GONZALES
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">JESUS ARIEL GONZALES
+								<p class="detalles">Semillero Developer & geeks</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/mujer.jpg" alt="" class="imagen">
+						<div class="nombre">
+							CELIA ANDREA SAAB
+							<p class="detalles">Tecnoparque</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">CELIA ANDREA SAAB
+								<p class="detalles">Tecnoparque</p>
+							</div>
+
+					</div>
+				</div>
+				<div class="container">
+					<img src="Vista/img/hombre.jpg" alt="" class="imagen">
+						<div class="nombre">
+							HUGO ALEXANDER SEMANTE
+							<p class="detalles">Semillero Developer & geeks</p>
+						</div>
+					<div class="overlay overlaybottom">
+						<br>
+							<a href="#"><span class="icon icon-instagram2"></span></a>
+							<a href="#"><span class="icon icon-social-facebook"></span></a>
+							<a href="#"><span class="icon icon-twitter"></span></a>
+							<div class="text">DANIEL MENDEZ
+								<p class="detalles">Semillero GIAT</p>
+							</div>
+
+					</div>
+				</div>
+
+			</div>
+		
+		</div>
+
+		<h2 class="compania">ESTE PROYECTO SE REALIZO EN COMPAÑIA DE:</h2>
+		<div class="izquierda">
+			<div class="conjunto">
+				<h1 class="entidad">SIDES</h1>
+				<p class="p"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. </p>
+			</div>
+		</div>
+		<div class="derecha">
+			<img class="sides" src="Vista/img/logo.png" >
+		</div>
+		<br>
+		<br>
+
+		<br>
+		<div class="sen">
+			<div class="izquierda2">
+				<img class="sena sides" src="Vista/img/sena.png" >
+				
+			</div>
+			<div class="derecha2">
+				<div class="conjunto">
+					<h1 class="entidad">SENA</h1>
+					<p class="p"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. </p>
+				</div>
+			</div>
+		</div>
+		<footer>
+			SIDES 2018 Copyright © Todos los derechos reservados.
+		</footer>
+	<script src="Vista/js/FuncionArriba.js"></script>
+</body>
+</html>
